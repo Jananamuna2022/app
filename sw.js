6df02c10ce240b18c48bd47eda1646948f79ae9a
Jananamuna2022 <tarathapa4352@gmail.com>
2025-04-27 21:43:52 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,52 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'jananamuna-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/1.jpg',
+  '/2.jpg',
+  '/3.jpg',
+  '/logo.png',
+  '/Jananamuna.apk'
+];
+
+// Install event: caching files
+self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Caching files');
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+// Activate event: cleaning old caches
+self.addEventListener('activate', (event) => {
   console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[Service Worker] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+// Fetch event: serving cache first, then network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached file OR fetch from network
+        return response || fetch(event.request);
+      })
+  );
 });
